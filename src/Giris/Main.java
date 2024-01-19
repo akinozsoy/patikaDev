34ba74d0ddb1386e51bb149ec77d8c91aa70822f
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-01-19 12:24:01 +0300

    patikaDevJavaStarted

---
+++ src/Giris/Main.java
@@ -0,0 +1,8 @@
+package Giris;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java 101 \n Patikası");
+        System.out.print("Başlıyoruz");
+    }
+}
