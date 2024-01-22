4b462c7cba146ed26142839d2febd68a865191f4
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-01-22 14:00:22 +0300

    patikaDevJavaStarted KDV

--- src/projects/KDV.java
+++ src/projects/KDV.java
@@ -1,8 +1,19 @@
 package projects;
-import java.util.Scanner;
+import java.util.Scanner; // KULLANICIDAN VERİ GİRİŞİ İÇİN KULLANILAN KÜTÜPHANE
 public class KDV {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+       double tutar,kdvoran = 0.18,kdvTutar,kdvliTutar; // %18 KDV TUTARI OLARAK ALINDI
+       Scanner input = new Scanner(System.in);// VERİ GİRİŞİ İÇİN ÇAĞIRILAN SCANNER KÜTÜPHANESİ
+        System.out.print("Ücret Tutarınız giriniz :");
+        tutar = input.nextDouble(); // İNPUT DEĞERİ 0.18 YANİ KDV ORANI %18 0LARAK GÖSTERİMİ
+        kdvTutar = tutar * kdvoran; // kdvTutar MARILEN TUTAR VE KDV ORANI İLE ÇARPIMI
+        kdvliTutar = tutar + kdvTutar;
+        System.out.println("KDV'siz Tutar : " +tutar);
+        System.out.println("KDV Oranı  : " +kdvoran);
+        System.out.println("KDV Tutarı  : " +kdvTutar);
+        System.out.println("KDV'li Tutarı  : " +kdvliTutar);
+
+
 
     }
 }
