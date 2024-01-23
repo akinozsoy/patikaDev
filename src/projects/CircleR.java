c36fcd46423ba7dfb44e0fc73b5705c62ff45619
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-01-23 09:27:05 +0300

    patikaDevJavaStarted TaksiMetre

---
+++ src/projects/CircleR.java
@@ -0,0 +1,17 @@
+package projects;
+import java.util.Scanner;
+
+public class CircleR {
+    public static void main(String[] args) {
+        int r;
+        double pi = 3.14;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dairenin alanını Giriniz ");
+        r = input.nextInt();
+        double alan= pi*r*r;
+        double cevre = 2 * pi * r;
+        System.out.println("Dairenin alanını: "+alan);
+        System.out.println("Dairenin Çevresi: " +cevre);
+
+    }
+}
--- src/projects/TriangleProject.java
+++ src/projects/TriangleProject.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 public class TriangleProject {
     public static void main(String[] args) {
         // Değişkenler
-        int a,b;
+        int a,b,alan;
         double c;
         // Kullanıcıdan Verilerimizi Alalım
         Scanner input = new Scanner(System.in);
         System.out.print("1. Kenarı Giriniz :");
         a = input.nextInt();
-        System.out.println("2. Kenarı Giriniz : ");
+        System.out.print("2. Kenarı Giriniz : ");
         b = input.nextInt();
         c = Math.sqrt((a*a)+(b*b));
-        System.out.println("Hipotenüs : "+ c );
+        System.out.println("Hipotenüs : "+c);
+        alan = a * b / 2;
+        System.out.println("Üçgenin alanı :" +alan);
+
+
+
+
     }
 }
