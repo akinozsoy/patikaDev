3029d3948861fbf8b9bf7173df9a782b7aea1f20
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-01-23 14:46:24 +0300

    patikaDevJavaStarted  Dairenin Alanını ve Çevresini Bulan Program

---
+++ src/projects/IndexCalculator.java
@@ -0,0 +1,15 @@
+package projects;
+import java.util.Scanner;
+
+public class IndexCalculator {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu giriniz(metre cinsinden) :");
+        double height = scanner.nextDouble();
+        System.out.println("Lütfen Kilonuzu giriniz(kg cinsinden) :");
+        double weight = scanner.nextDouble();
+        double bmi = weight / (height * height);
+        System.out.println("Vücut Kitle İndeksiniz : "+bmi);
+        scanner.close();
+    }
+}
