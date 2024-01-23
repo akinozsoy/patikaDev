b153dfecc8db87c8dfa73a7c3c9c5dbbda56015d
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-01-23 09:01:13 +0300

    patikaDevJavaStarted ÜÇGEN

---
+++ src/projects/TaksiMetre.java
@@ -0,0 +1,20 @@
+package projects;
+import java.util.Scanner;
+
+public class TaksiMetre {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20,startPrice = 10,total;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi KM Cinsinden giriniz : ");
+        km = input.nextInt();
+        total = (perKm * km);
+        total +=startPrice;
+        total =(total<20) ? 20 : total;
+        System.out.println("Ücretiniz : " +total);
+
+
+
+    }
+}
+
