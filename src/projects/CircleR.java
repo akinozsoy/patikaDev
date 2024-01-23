2b162ab858742ce4d9f3e072a75c790e33bc97da
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-01-23 14:06:46 +0300

    patikaDevJavaStarted  Dairenin Alanını ve Çevresini Bulan Program

--- src/projects/CircleR.java
+++ src/projects/CircleR.java
@@ -7,11 +7,15 @@ public class CircleR {
         double pi = 3.14;
         Scanner input = new Scanner(System.in);
         System.out.println("Dairenin alanını Giriniz ");
+        System.out.println("Yarıçapını Giriniz");
         r = input.nextInt();
         double alan= pi*r*r;
         double cevre = 2 * pi * r;
         System.out.println("Dairenin alanını: "+alan);
         System.out.println("Dairenin Çevresi: " +cevre);
+
+        //--------------------------------------------------------//
+
 
     }
 }
