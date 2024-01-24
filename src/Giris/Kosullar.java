255d19b9b3ec7cd9eb71b67d0ce898800b45908d
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-01-24 10:49:25 +0300

    Kosullar.java

---
+++ src/Giris/Kosullar.java
@@ -0,0 +1,19 @@
+package Giris;
+
+public class Kosullar {
+    public static void main(String[] args) {
+        int a = 10, b = 10, c ;
+        c = (a == b) ? 2:3;
+
+        boolean kosul1 = (a < b);
+        boolean kosul2 = (a > c);
+        boolean sonuc = kosul1 || kosul2;
+        System.out.println(kosul1);
+        System.out.println(kosul2);
+        System.out.println(!sonuc);
+        System.out.println(c);
+
+
+
+    }
+}
