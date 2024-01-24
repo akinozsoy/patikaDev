32c32d9833eb8c1a8476befd48ee2316d70d1718
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-01-24 10:37:16 +0300

    Operators

---
+++ src/Giris/Operators.java
@@ -0,0 +1,46 @@
+package Giris;
+
+public class Operators {
+    public static void main(String[] args) {
+        int A = 10 , B = 20 , C = 10 , D= 40;
+        // == Operatörü
+        System.out.println(A == B);
+        System.out.println(A == C);
+        System.out.println(C == D);
+
+        // != Eşit Değil Operatörü
+
+        System.out.println(A != D);
+        System.out.println(A != C);
+        System.out.println(C != B);
+
+        // > Büyüktür Operatörü
+
+        System.out.println(A > D);
+        System.out.println(D > C);
+        System.out.println(C > B);
+
+        // >= Büyük-Eşittir Operatörü
+
+        System.out.println(A >= D);
+        System.out.println(A >= C);
+        System.out.println(C >= B);
+
+        // < Küçüktür Operatörü
+
+        System.out.println(A < D);
+        System.out.println(D < C);
+        System.out.println(C < B);
+
+
+        // <= Küçük-Eşittir Operatörü
+
+        System.out.println(A <= D);
+        System.out.println(A <= C);
+        System.out.println(C <= B);
+
+        // -----------------------------------
+
+
+    }
+}
