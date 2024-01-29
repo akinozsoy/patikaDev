e4c6e27913bc9b1bb1880f66bf95958d7b43bad6
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-01-29 17:18:37 +0300

    Zodyak Yılı

---
+++ src/projects/ChinaZodiac.java
@@ -0,0 +1,53 @@
+package projects;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ChinaZodiac {
+    public static void main(String[] args) {
+        int year,mod = 12;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Doğum Yılınızı Giriniz :");
+        year = scanner.nextInt();
+        year %= mod;
+        switch (year){
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz : Koyun");
+                break;
+            default:
+                System.out.println("Yanlış Yıl Girdiniz");
+        }
+    }
+}
