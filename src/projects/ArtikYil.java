ac25b465197dbce43abb11bac682dabfc0d947a7
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-02-01 14:35:25 +0300

    Zodyak Yılı

---
+++ src/projects/ArtikYil.java
@@ -0,0 +1,18 @@
+package projects;
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Yılı Giriniz :");
+        int yil  = input.nextInt();
+        if (yil % 4 == 0 && (yil % 100 != 0 || yil % 400 == 0))
+        {
+            System.out.println(yil + " artık yıldır.");
+        }
+        else
+        {
+            System.out.println(yil + " artık yıl değildir.");
+        }
+    }
+}
