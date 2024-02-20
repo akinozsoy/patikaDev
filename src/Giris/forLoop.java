59fbd742bbed0f77f51e70dbe77d3dd08bc01271
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-02-20 16:52:28 +0300

    .

--- src/Giris/forLoop.java
+++ src/Giris/forLoop.java
@@ -6,5 +6,10 @@ public class forLoop {
         for(int sayac = 3; sayac <= 10; sayac+= 2){
             System.out.println(sayac);
         }
+        int sum = 0;
+        for (int i = 0, j = 0; i < 5 || j < 5; ++i, j = i + 1) {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
---
+++ src/Giris/whileFor.java
@@ -0,0 +1,22 @@
+package Giris;
+import java.util.Scanner;
+
+public class whileFor {
+    public static void main(String[] args) {
+        /*
+        Scanner input = new Scanner(System.in);
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println(i);
+
+        }
+
+         */
+        int i = 1;
+        while (i <= 10){
+            System.out.println(i);
+            i++;
+        }
+
+
+    }
+}
